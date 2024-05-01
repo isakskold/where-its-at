fbee83dac0b36051649523e6b8310accdc9efabf
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-05-01 13:12:48 +0200

    minor push

--- vite-project/src/pages/ComponentDevelopment.jsx
+++ vite-project/src/pages/ComponentDevelopment.jsx
@@ -1,8 +1,6 @@
 import Counter from "../components/counter/Counter";
 
 const ComponentDevelopment = () => {
-  const initialData = "Data";
-
   return (
     <>
       <Counter initialData={"Money"} />
