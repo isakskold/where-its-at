8cd2330f1471c09ab442a593e76edea34dbd4d70
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-05-02 04:09:40 +0200

    Added comment explaining the use of ComponentDevelopment

--- vite-project/src/pages/ComponentDevelopment.jsx
+++ vite-project/src/pages/ComponentDevelopment.jsx
@@ -2,6 +2,8 @@ import Event from "./event/Event";
 import Events from "./events/Events";
 import Order from "./order/Order";
 
+// Test enviornment. Render the pages for visualisation. Will not be used when routing is set up.
+
 const ComponentDevelopment = () => {
   return (
     <>
